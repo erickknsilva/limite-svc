356c5da29ced882c16f0d7ef6d93834b045b7b04
erickknsilva <erick.nsilva100@gmail.com>
2025-05-05 17:46:22 -0300

    Dependencias iniciais do aplicativo

---
+++ src/main/java/com/coffeandit/limitesvc/LimiteSvcApplication.java
@@ -0,0 +1,13 @@
+package com.coffeandit.limitesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimiteSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimiteSvcApplication.class, args);
+	}
+
+}
